e1d7ed83e343db7bad5839b19a6292be53f2d24a
cspoppe <cspoppe@gmail.com>
2022-06-01 15:34:16 -0700

    Update home.js

--- static/home.js
+++ static/home.js
@@ -152,7 +152,7 @@ const messageWindow = {
                                     ----- Websockets -----
 **************************************************************************************************/
 
-socket = io('https://' + location.host);
+socket = io('wss://' + location.host);
 
 socket.on('connect', () => {
     socket.emit('home', {});
